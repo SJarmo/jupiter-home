90806a25cd7797454863316e16299757f1d158fd
Jarmo Sepp <jarmo.sepp@yahoo.com>
2025-07-17 11:05:24 +0300

    Jupiter WIP

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,6 +13,8 @@ import { CommonModule } from '@angular/common';
 export class App implements OnInit {
   protected title = 'JUPITER';
   categories: any[] | null = null;
+  showLeftArrow = false;
+  showRightArrow = true;
 
   constructor(private errService: ErrService) {}
 
@@ -25,8 +27,28 @@ export class App implements OnInit {
       error: (err) => console.error(err)
     });
   }
-
   scrollRight(element: HTMLElement) {
-    element.scrollBy({ left: 200, behavior: 'smooth' });
+    const firstCard = element.querySelector('.video-card') as HTMLElement;
+    if (firstCard) {
+      const cardWidth = firstCard.offsetWidth + 10;
+      element.scrollBy({ left: cardWidth * 5, behavior: 'smooth' });
+      this.updateArrows(element);
+      setTimeout(() => this.updateArrows(element), 300);
+    }
   }
+
+  scrollLeft(element: HTMLElement) {
+    const firstCard = element.querySelector('.video-card') as HTMLElement;
+    if (firstCard) {
+      const cardWidth = firstCard.offsetWidth + 10;
+      element.scrollBy({ left: -(cardWidth * 5), behavior: 'smooth' });
+      setTimeout(() => this.updateArrows(element), 300);
+    }
+  }
+  updateArrows(element: HTMLElement) {
+    const maxScrollLeft = element.scrollWidth - element.clientWidth;
+    this.showLeftArrow = element.scrollLeft > 0;
+    this.showRightArrow = element.scrollLeft < maxScrollLeft - 1; // -1, et jääks väike varu
+  }
+
 }
